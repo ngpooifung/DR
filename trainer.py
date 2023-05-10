5007afe9d4dcf7e8c32dc4215ebd8e768094c0a3
pwuaj <pwuaj@ecelvd406.ece.local>
2023-05-10 21:45:22 +0800

    udpate

--- trainer.py
+++ trainer.py
@@ -49,11 +49,11 @@ class Classictrainer(object):
         train_features, train_labels = self.get_features(train_loader)
         test_features, test_labels = self.get_features(test_loader)
 
-        if args.use_mlp:
+        if self.args.use_mlp:
             classifier = MLPClassifier(max_iter = 10000)
         else:
             classifier=LogisticRegression(max_iter=10000)
-            
+
         classifier.fit(train_features, train_labels)
 
         # Evaluate using the logistic regression classifier
