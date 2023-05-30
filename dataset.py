add4347c515279b2b9e1ef810a7fcffc5b13b015
pwuaj <pwuaj@ecelvd406.ece.local>
2023-05-31 01:08:47 +0800

    udpate

--- dataset.py
+++ dataset.py
@@ -75,6 +75,7 @@ class Modeldataset:
         data_transforms = transforms.Compose([
                                               # transforms.RandomResizedCrop(size=size, scale = (0.6,1.0)),
                                               transforms.RandomHorizontalFlip(),
+                                              transforms.RandomVerticalFlip(),
                                               # RotationTransform(angles=[0, 90, 180, 270])
                                               # transforms.RandomRotation(degrees = (0,180))
                                               ])
