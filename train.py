508512ca5fd122d224fd025caabdf1dd70e72253
pwuaj <pwuaj@ecelvd406.ece.local>
2023-06-05 12:35:02 +0800

    udpate

--- train.py
+++ train.py
@@ -90,14 +90,14 @@ def combine():
     clip_model, _ = clip.load('ViT-L/14@336px', device = args.device)
     n_px = clip_model.visual.input_resolution
     clip_model.ffn = None
-    path = os.path.join('RDRlong', 'Cliplayertune/Cliplayertune_0200.pth.tar')
+    path = os.path.join('VTDRlong', 'Cliplayertune/Cliplayertune_0200.pth.tar')
     checkpoint = torch.load(path, map_location = args.device)
     state_dict = checkpoint['state_dict']
     clip_model.load_state_dict(state_dict, strict=True)
     clip_model = clip_model.to(args.device)
 
     model = modeltrainer()._get_model(base_model = 'resnet50', out_dim = 2)
-    path = os.path.join('RDRlong', 'main/main_0100.pth.tar')
+    path = os.path.join('VTDRlong', 'main/main_0100.pth.tar')
     checkpoint = torch.load(path, map_location = args.device)
     state_dict = checkpoint['state_dict']
     model.load_state_dict(state_dict, strict=True)
