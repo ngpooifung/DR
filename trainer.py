d33484306d1dc0ff178c6c09a84c3af5a0494e5c
pwuaj <pwuaj@ecelvd406.ece.local>
2023-06-05 13:26:10 +0800

    udpate

--- trainer.py
+++ trainer.py
@@ -44,9 +44,9 @@ class Comtrainer(object):
         test_features, test_labels = self.get_features(test_loader)
 
         if self.args.use_mlp:
-            classifier = MLPClassifier(max_iter = 100000)
+            classifier = MLPClassifier(hidden_layer_sizes=(512,), max_iter = 10000)
         else:
-            classifier=LogisticRegression(max_iter=100000)
+            classifier=LogisticRegression(max_iter=10000)
 
         classifier.fit(train_features, train_labels)
 
