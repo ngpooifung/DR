bfb0daf9a22b7b3223b886f07ba4956e527335ff
pwuaj <pwuaj@ecelvd406.ece.local>
2023-06-05 17:46:50 +0800

    update

--- trainer.py
+++ trainer.py
@@ -44,7 +44,7 @@ class Comtrainer(object):
         test_features, test_labels = self.get_features(test_loader)
 
         if self.args.use_mlp:
-            classifier = MLPClassifier(hidden_layer_sizes=(self.args.hidden,), max_iter = 10000, learning_rate_init = self.args.lr, solver = 'adam', batch_size = 'auto')
+            classifier = MLPClassifier(max_iter = 10000)
         else:
             classifier=LogisticRegression(max_iter=10000)
 
