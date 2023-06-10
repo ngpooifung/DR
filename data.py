42a971d1aac429e8e9ff7d8e7618f2dac3a37b84
pwuaj <pwuaj@ecelvd406.ece.local>
2023-06-10 16:49:47 +0800

    udpate

--- data.py
+++ data.py
@@ -25,95 +25,41 @@ import shutil
 #
 # gradable.to_csv('./gradable.csv')
 # ungradable.to_csv('./ungradable.csv')
-#
-#
-# # %% folder
-# img_dir = '/scratch/PI/eeaaquadeer/UWF'
-# output_dir = '/scratch/PI/eeaaquadeer/Adam'
-# ungrad_dir = '/scratch/PI/eeaaquadeer/Adam/ungradable'
-# folder = os.listdir(img_dir)
-# classes = ['VTDR', 'RDR']
-#
-# for name in folder:
-#     if os.path.exists(os.path.join(*[img_dir, name, 'REDGREEN'])):
-#         dir = os.path.join(*[img_dir, name, 'REDGREEN'])
-#     else:
-#         dir = os.path.join(*[img_dir, name])
-#     imglist = os.listdir(dir)
-#     for img in imglist:
-#
-#         if '(' in img or (img[-3:]!="tif" and img[-3:]!="jpg"):
-#             continue
-#         image = cv2.imread(os.path.join(*[dir, img]))
-#         height, width, channels = image.shape
-#         if height<500 or width<500 or channels!=3 or image[:,:,0].sum()==image[:,:,1].sum():
-#             continue
-#         image = cv2.resize(image, (960, 768))
-#
-#         if img.split('.')[0] in list(gradable['IMG']):
-#             index = gradable.index[gradable['IMG'] == img.split('.')[0]]
-#             if len(index)>1:
-#                 continue
-#             for cla in classes:
-#                 label = gradable[cla][index.item()]
-#                 tifffile.imsave(os.path.join(*[output_dir, cla, str(label), img.split('.')[0] + '.tif']), image)
-#
-#         elif img.split('.')[0] in list(ungradable['IMG']):
-#             tifffile.imsave(os.path.join(*[ungrad_dir, img.split('.')[0] + '.tif']), image)
-
-# # %% jpd to tiff
-# jpg_dir = '/scratch/PI/eeaaquadeer/Phoom/RDR'
-# tiff_dir = '/scratch/PI/eeaaquadeer/Phoom/RDRt'
-# split = ['test', 'training', 'validation']
-# classes = ['0', '1']
-# for sp in split:
-#     for cla in classes:
-#         imglist = os.listdir(os.path.join(*[jpg_dir, sp, cla]))
-#         for img in imglist:
-#             image = cv2.imread(os.path.join(*[jpg_dir, sp, cla, img]))
-#             tifffile.imsave(os.path.join(*[tiff_dir, sp, cla, img.split('.')[0] + '.tif']), image)
 
 
-# %% read Phoom accuracy
-sp = 'test'
-csv = '/home/pwuaj/hkust/Phoom/vtdrprobs.csv'
-csv = pd.read_csv(csv)
-predict = []
-for i in range(sum(csv['Split'] == sp)):
-    predict.append(float(csv['Predicted Probability'][csv['Split'] == sp].iloc[i][1:5]))
-predict = np.array(predict)
-label = np.array(csv['Label'][csv['Split'] == sp])
-predict = (predict >= 0.43)*1
-print(classification_report(label, predict, digits = 4))
-#
-#
-# %% read excel accuracy
-csv = '/home/pwuaj/hkust/DR/All_20200715 emma_oneeye.xlsx'
-csv = pd.read_excel(csv)
-predict = []
-label = []
-for i in range(408): # 397 408
-    predict.append(csv['Pred_V0'][i])
-    label.append(int(csv['Label_V0'][i]))
-predict = np.array(predict)
-label = np.array(label)
-predict = (predict >= 0.5)*1
-print(classification_report(label, predict, digits = 4))
-#
-# # %%
-# threshold = []
-# accuracy = []
-# for p in np.unique(predict):
-#   threshold.append(p)
-#   y_pred = (predict >= p).astype(int)
-#   accuracy.append(balanced_accuracy_score(label,y_pred))
-# threshold[np.argmax(accuracy)]
-#
-# %%
-from sklearn.metrics import roc_curve
-fpr, tpr, thresholds = roc_curve(label, predict, drop_intermediate = False)
-# thresholds[np.argmin(np.abs(fpr+tpr-1))]
-thresholds[np.argmax(tpr-fpr)]
+# # %% read Phoom accuracy
+# sp = 'test'
+# csv = '/home/pwuaj/hkust/Phoom/vtdrprobs.csv'
+# csv = pd.read_csv(csv)
+# predict = []
+# for i in range(sum(csv['Split'] == sp)):
+#     predict.append(float(csv['Predicted Probability'][csv['Split'] == sp].iloc[i][1:5]))
+# predict = np.array(predict)
+# label = np.array(csv['Label'][csv['Split'] == sp])
+# predict = (predict >= 0.43)*1
+# print(classification_report(label, predict, digits = 4))
+
+
+# # %% read excel accuracy
+# csv = '/home/pwuaj/hkust/DR/All_20200715 emma_oneeye.xlsx'
+# csv = pd.read_excel(csv)
+# predict = []
+# label = []
+# for i in range(408): # 397 408
+#     predict.append(csv['Pred_V0'][i])
+#     label.append(int(csv['Label_V0'][i]))
+# predict = np.array(predict)
+# label = np.array(label)
+# predict = (predict >= 0.5)*1
+# print(classification_report(label, predict, digits = 4))
+
+
+# # %% Youden
+# from sklearn.metrics import roc_curve
+# fpr, tpr, thresholds = roc_curve(label, predict, drop_intermediate = False)
+# # thresholds[np.argmin(np.abs(fpr+tpr-1))]
+# thresholds[np.argmax(tpr-fpr)]
+
 
 # # %% read Phoom gradtest
 # csv = 'skprobs.csv'
@@ -131,8 +77,7 @@ thresholds[np.argmax(tpr-fpr)]
 # print(classification_report(label, predict, digits = 4))
 
 
-# # %%
-# # read sensitivity
+# # %% read sensitivity
 # csv = '/home/pwuaj/hkust/DR/test.csv'
 # csv = pd.read_csv(csv)
 # predict = np.array(csv['Predicted label'])
@@ -140,31 +85,16 @@ thresholds[np.argmax(tpr-fpr)]
 # print(classification_report(label, predict, digits = 4))
 
 
-# %%
-csv = '/home/pwuaj/hkust/DR/All_20200715 emma_oneeye.xlsx'
-csv = pd.read_excel(csv)
-rdr = '/scratch/PI/eeaaquadeer/Phoom/RDRlong/test'
-rdrtest = '/scratch/PI/eeaaquadeer/Phoom/RDRtrue2/test'
-vtdr = '/scratch/PI/eeaaquadeer/Phoom/VTDRlong/test'
-vtdrtest = '/scratch/PI/eeaaquadeer/Phoom/VTDRtrue2/test'
+# %% SK data
+csv = pd.read_csv('/home/pwuaj/hkust/DR/skprobs.csv')
 for i in range(len(csv)):
-    name = csv['Image_R0 thr_0.66'][i]
-    if isinstance(name, str):
-        store_lbl = int(csv['Label_R0'][i])
-        save_lbl = int(csv['Label_R0'][i])
-        try:
-            shutil.copyfile(os.path.join(*[rdr, str(store_lbl), name]), os.path.join(*[rdrtest, str(save_lbl), name]))
-        except:
-            shutil.copyfile(os.path.join(*[rdr, str(1-store_lbl), name]), os.path.join(*[rdrtest, str(save_lbl), name]))
-            print(name)
-
-for i in range(len(csv)):
-    name = csv['Image_V0'][i]
-    if isinstance(name, str):
-        store_lbl = int(csv['Label_V0'][i])
-        save_lbl = int(csv['Label_V0'][i])
-        try:
-            shutil.copyfile(os.path.join(*[vtdr, str(store_lbl), name]), os.path.join(*[vtdrtest, str(save_lbl), name]))
-        except:
-            shutil.copyfile(os.path.join(*[vtdr, str(1-store_lbl), name]), os.path.join(*[vtdrtest, str(save_lbl), name]))
-            print(name)
+    name = csv.loc[i]['Image_Name']
+    folder = csv.loc[i]['MRD']
+    RDR = csv.loc[i]['Referable']
+    VTDR = csv.loc[i]['VTDR']
+    Ungradable = csv.loc[i]['Ungradable Label']
+    if Ungradable == 1:
+        shutil.copyfile(os.path.join(*['SK_data', folder, name]), os.path.join(*['SK/ungradable', name]))
+    elif Ungradable == 0:
+        shutil.copyfile(os.path.join(*['SK_data', folder, name]), os.path.join(*['SK/gradable/RDR', str(RDR), name]))
+        shutil.copyfile(os.path.join(*['SK_data', folder, name]), os.path.join(*['SK/gradable/VTDR', str(VTDR), name]))
