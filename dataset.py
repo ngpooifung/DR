807db32b943c6791df87bc3761ef8ffcfd133ed3
pwuaj <pwuaj@ecelvd406.ece.local>
2023-06-13 16:53:52 +0800

    update

--- dataset.py
+++ dataset.py
@@ -56,7 +56,8 @@ class Imagefolder(datasets.ImageFolder):
                                               # CenterCrop(self.resize),
                                               _convert_image_to_rgb,
                                               ToTensor(),
-                                              # Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711)),])
+                                              # Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711)),]
+                                              )
         if self.preprocess:
             img = data_transforms(img)
         if self.transform is not None:
