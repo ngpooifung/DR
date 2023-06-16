d411f6216847c522673f6480977da78d3e85a186
pwuaj <pwuaj@ecelvd406.ece.local>
2023-06-16 13:21:13 +0800

    update

--- dataset.py
+++ dataset.py
@@ -53,7 +53,6 @@ class Imagefolder(datasets.ImageFolder):
         #     raise ValueError(f'Size={self.size} > {shape[0]},{shape[1]}')
         # img = img.astype('float32')
         data_transforms = transforms.Compose([Resize((self.resize, int(self.resize*1.25)), interpolation=BICUBIC),
-                                              GaussianBlur((7,9)),
                                               # CenterCrop(self.resize),
                                               _convert_image_to_rgb,
                                               ToTensor(),
@@ -76,6 +75,7 @@ class Modeldataset:
         data_transforms = transforms.Compose([
                                               # transforms.RandomResizedCrop(size=size, scale = (0.6,1.0)),
                                               transforms.RandomHorizontalFlip(),
+                                              GaussianBlur((7,9)),
                                               # transforms.RandomVerticalFlip(),
                                               # RotationTransform(angles=[0, 90, 180, 270])
                                               # transforms.RandomRotation(degrees = (0,180))
