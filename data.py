00648c0b746bf8cec9c6d119089deebe08c69cf1
pwuaj <pwuaj@ecelvd406.ece.local>
2023-07-03 04:21:21 +0800

    update

--- data.py
+++ data.py
@@ -7,6 +7,7 @@ import tifffile
 import cv2
 from sklearn.metrics import classification_report, balanced_accuracy_score
 import shutil
+import matplotlib.pyplot as plt
 # # %%
 # csv_dir = './H.csv'
 # csv = pd.read_csv(csv_dir)
@@ -81,38 +82,82 @@ import shutil
 # print(classification_report(label, predict, digits = 4))
 
 
-# %% read sensitivity
-from sklearn.metrics import roc_curve
-csv = '/home/pwuaj/hkust/DR/test.csv'
-csv = pd.read_csv(csv)
-predict = np.array(csv['Probability'])
-label = np.array(csv['True label'])
-fpr, tpr, thresholds = roc_curve(label, predict, drop_intermediate = False)
-th = thresholds[np.argmax(tpr-fpr)]
-predict = (predict >= th)*1
-print(th,classification_report(label, predict, digits = 4))
+# # %% read sensitivity
+# from sklearn.metrics import roc_curve
+# csv = '/home/pwuaj/hkust/DR/test.csv'
+# csv = pd.read_csv(csv)
+# predict = np.array(csv['Probability'])
+# # plt.hist(predict, bins = 50)
+# label = np.array(csv['True label'])
+# fpr, tpr, thresholds = roc_curve(label, predict, drop_intermediate = False)
+# th = thresholds[np.argmax(tpr-fpr)]
+# predict = (predict >= th)*1
+# print(th,classification_report(label, predict, digits = 4))
 
-# %% SK data
-RDRnames = []
-RDRfolders = []
-for root, dir, file in os.walk('/home/pwuaj/data/RDRtrue'):
-    for name in file:
-        RDRnames.append(name)
-        RDRfolders.append(root)
-VTDRnames = []
-VTDRfolders = []
-for root, dir, file in os.walk('/home/pwuaj/data/VTDRtrue'):
-    for name in file:
-        VTDRnames.append(name)
-        VTDRfolders.append(root)
-print(len(RDRnames), len(VTDRnames))
-for root, dir, file in os.walk('/home/pwuaj/data/RDRraw'):
-    for name in file:
-        if name in RDRnames:
-            folder = RDRfolders[RDRnames.index(name)].split('/')
-            shutil.copyfile(os.path.join(*[root, name]), os.path.join(*['/home/pwuaj/data/RDRrawtrue', folder[-2], folder[-1], name]))
-for root, dir, file in os.walk('/home/pwuaj/data/VTDRraw'):
-    for name in file:
-        if name in VTDRnames:
-            folder = VTDRfolders[VTDRnames.index(name)].split('/')
-            shutil.copyfile(os.path.join(*[root, name]), os.path.join(*['/home/pwuaj/data/VTDRrawtrue', folder[-2], folder[-1], name]))
+# # %% SK data
+# RDRnames = []
+# RDRfolders = []
+# for root, dir, file in os.walk('/home/pwuaj/data/RDRtrue'):
+#     for name in file:
+#         RDRnames.append(name)
+#         RDRfolders.append(root)
+# VTDRnames = []
+# VTDRfolders = []
+# for root, dir, file in os.walk('/home/pwuaj/data/VTDRtrue'):
+#     for name in file:
+#         VTDRnames.append(name)
+#         VTDRfolders.append(root)
+# print(len(RDRnames), len(VTDRnames))
+# for root, dir, file in os.walk('/home/pwuaj/data/RDRraw'):
+#     for name in file:
+#         if name in RDRnames:
+#             folder = RDRfolders[RDRnames.index(name)].split('/')
+#             shutil.copyfile(os.path.join(*[root, name]), os.path.join(*['/home/pwuaj/data/RDRrawtrue', folder[-2], folder[-1], name]))
+# for root, dir, file in os.walk('/home/pwuaj/data/VTDRraw'):
+#     for name in file:
+#         if name in VTDRnames:
+#             folder = VTDRfolders[VTDRnames.index(name)].split('/')
+#             shutil.copyfile(os.path.join(*[root, name]), os.path.join(*['/home/pwuaj/data/VTDRrawtrue', folder[-2], folder[-1], name]))
+
+# %% RDR VTDR
+a = ['RDR', 'VTDR']
+for i in a:
+    root = os.path.join(*['/home/pwuaj/data/SK/gradable', i])
+    classes = ['0', '1']
+    for c in classes:
+        files = os.listdir(os.path.join(*[root, c]))
+        train = round(len(files)*16/25)
+        valid = round(len(files)*4/25)
+        test = round(len(files)*1/5)
+        random.shuffle(files)
+        trainlist = files[:train]
+        validlist = files[train:train+valid]
+        testlist = files[train+valid:]
+        for l in trainlist:
+            shutil.copyfile(os.path.join(*[root, c, l]), os.path.join(*['/home/pwuaj/data', i, 'training', c, l]))
+        for l in validlist:
+            shutil.copyfile(os.path.join(*[root, c, l]), os.path.join(*['/home/pwuaj/data', i, 'validation', c, l]))
+        for l in testlist:
+            shutil.copyfile(os.path.join(*[root, c, l]), os.path.join(*['/home/pwuaj/data/SKtest', i, c, l]))
+            shutil.copyfile(os.path.join(*[root, c, l]), os.path.join(*['/home/pwuaj/data', i, 'test', c, l]))
+
+a = ['RDR', 'VTDR']
+for i in a:
+    root = os.path.join(*['/home/pwuaj/data/GEI/gradable', i])
+    classes = ['0', '1']
+    for c in classes:
+        files = os.listdir(os.path.join(*[root, c]))
+        train = round(len(files)*16/25)
+        valid = round(len(files)*4/25)
+        test = round(len(files)*1/5)
+        random.shuffle(files)
+        trainlist = files[:train]
+        validlist = files[train:train+valid]
+        testlist = files[train+valid:]
+        for l in trainlist:
+            shutil.copyfile(os.path.join(*[root, c, l]), os.path.join(*['/home/pwuaj/data', i, 'training', c, l]))
+        for l in validlist:
+            shutil.copyfile(os.path.join(*[root, c, l]), os.path.join(*['/home/pwuaj/data', i, 'validation', c, l]))
+        for l in testlist:
+            shutil.copyfile(os.path.join(*[root, c, l]), os.path.join(*['/home/pwuaj/data/GEItest', i, c, l]))
+            shutil.copyfile(os.path.join(*[root, c, l]), os.path.join(*['/home/pwuaj/data', i, 'test', c, l]))
