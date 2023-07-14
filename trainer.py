f8d3a3d0a3d23d322c1f085099c1745d0cbc855d
pwuaj <pwuaj@ecelvd406.ece.local>
2023-07-14 16:25:40 +0800

    update

--- trainer.py
+++ trainer.py
@@ -277,3 +277,4 @@ class Restrainer(object):
                 plt.imshow(np.asarray(image).squeeze().transpose(1,2,0))
                 plt.imshow(final_cam.squeeze().detach().cpu().numpy(), alpha=0.3)
                 plt.savefig(os.path.join(*['/home/pwuaj/data/cam', name]))
+                plt.close()
