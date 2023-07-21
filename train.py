af9d87b553036aecdc07dbfe26ee066f94a5c994
pwuaj <pwuaj@ecelvd406.ece.local>
2023-07-21 22:23:21 +0800

    update

--- train.py
+++ train.py
@@ -150,7 +150,7 @@ def eval():
 
     model = modeltrainer()._get_model(base_model = args.arch, out_dim = args.out_dim).to(args.device)
     # if args.process == 'class_activation':
-    model.backbone.fc = nn.Linear(model.backbone.fc[0].in_features, 2)
+    # model.backbone.fc = nn.Linear(model.backbone.fc[0].in_features, 2)
     log = model.load_state_dict(state_dict, strict=True)
     model = model.to(args.device)
     model = DDP(model, device_ids = [local_rank], output_device=local_rank)
