891e269524b50cd0966973407557f4b67dd0dcce
pwuaj <pwuaj@ecelvd406.ece.local>
2023-10-17 08:22:58 +0800

    update

--- dataset.py
+++ dataset.py
@@ -75,7 +75,7 @@ class Modeldataset:
     def get_transform(size):
         data_transforms = transforms.Compose([
                                               # transforms.RandomResizedCrop(size=size, scale = (0.6,1.0)),
-                                              # transforms.RandomHorizontalFlip(),
+                                              transforms.RandomHorizontalFlip(p = 0.3),
                                               # transforms.RandomRotation(degrees = 2),
                                               # RandomAffine(degrees = 5),
                                               # ColorJitter(0.01, 0.01),
