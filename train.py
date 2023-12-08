32be799ee7d5a05127ee0cdcc564318b7d95081d
pwuaj <pwuaj@ecelvd406.ece.local>
2023-12-09 00:47:53 +0800

    update

--- train.py
+++ train.py
@@ -107,11 +107,11 @@ def main():
         checkpoint = torch.load(path, map_location = args.device)
         state_dict = checkpoint['state_dict']
         log = model.load_state_dict(state_dict, strict=False)
-        model.backbone.fc[3] = nn.Linear(64, 2)
+        model.backbone.fc[3] = nn.Linear(64, 2).to(args.device)
         print(log)
-        for name, param in model.named_parameters():
-            if name not in ['backbone.fc.0.weight', 'backbone.fc.0.bias', 'backbone.fc.3.weight', 'backbone.fc.3.bias']:
-                param.requires_grad = False
+        # for name, param in model.named_parameters():
+        #     if name not in ['backbone.fc.0.weight', 'backbone.fc.0.bias', 'backbone.fc.3.weight', 'backbone.fc.3.bias']:
+        #         param.requires_grad = False
 
         model = model.to(args.device)
     # model,_ = clip.load('RN50', device = args.device)
