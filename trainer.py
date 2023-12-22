b340caa064b8a592f4995c0ad01cf13a2d5d463f
pwuaj <pwuaj@ecelvd406.ece.local>
2023-12-23 02:12:53 +0800

    update

--- trainer.py
+++ trainer.py
@@ -187,7 +187,7 @@ class Restrainer(object):
 
                 logits = self.model.module(img.type(self.dtype))
                 loss = self.criterion(logits.logits.squeeze(), lbl)
-                print(logits.shape)
+                print(logits.logtis.shape)
 
                 top1 = topacc(logits, lbl, topk = (1,))
                 # top1 = accuracy_score(lbl.cpu(), (logits>0.5).cpu())
