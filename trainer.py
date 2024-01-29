7f22bec1826736c901415e63c28cd6dfb32addc8
pwuaj <pwuaj@ecelvd406.ece.local>
2024-01-29 13:49:10 +0800

    update

--- trainer.py
+++ trainer.py
@@ -313,3 +313,9 @@ class Restrainer(object):
                     plt.imshow(final_cam.squeeze().detach().cpu().numpy(), alpha=0.3, cmap = self.args.cmap)
                     plt.savefig(os.path.join(*['/home/pwuaj/data/cam', name]))
                     plt.close()
+                if lbl.item() == 0:
+                    plt.figure()
+                    plt.imshow(np.asarray(image).squeeze().transpose(1,2,0))
+                    plt.imshow(final_cam.squeeze().detach().cpu().numpy(), alpha=0.3, cmap = self.args.cmap)
+                    plt.savefig(os.path.join(*['/home/pwuaj/data/cam2', name]))
+                    plt.close()
