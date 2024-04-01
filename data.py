05ce9072886540d51f272d9137da2657fe411c44
pwuaj <pwuaj@ecelvd406.ece.local>
2024-04-01 22:36:08 +0800

    update

--- data.py
+++ data.py
@@ -141,6 +141,7 @@ csv = pd.read_csv(csv)
 for i in range(len(csv)):
     name = csv['image'].iloc[i] + '.csv'
     severity = csv['level'].iloc[i]
+    print(name, severity)
     RDR = int(severity>=2)*1
     VTDR = int(severity>=3)*1
     shutil.copy(os.path.join*['/home/pwuaj/data/kaggle', name], os.path.join(*['/home/pwuaj/data/fundus/RDR', str(RDR), name]))
