75d77fb8755fe33461c6698e4034dcdf63a58e93
pwuaj <pwuaj@ecelvd406.ece.local>
2024-04-23 13:06:10 +0800

    update

--- runwrapper.py
+++ runwrapper.py
@@ -1,7 +1,7 @@
 from wrapper import Gradwrapper, RDRwrapper, VTDRwrapper
 a,b,c,d = Gradwrapper('/home/pwuaj/data/grad/training/1/STDR052-20180323@101729-L1-S.jpg')
 print(a,b,c,d)
-a,b,c,d = RDRwrapper('/home/pwuaj/data/grad/validation/1/STDR052-20170911@105117-L1-S.jpg')
+a,b,c,d = RDRwrapper('/home/pwuaj/data/RDRraw/test/1/STDR365-20180621@162653-L4-S.jpg')
 print(a,b,c,d)
-a,b,c,d = VTDRwrapper('/home/pwuaj/data/grad/validation/1/STDR052-20170911@105117-L1-S.jpg')
+a,b,c,d = VTDRwrapper('/home/pwuaj/data/VTDRraw/test/0/STDR361-20180521@110114-R12-S.jpg')
 print(a,b,c,d)
