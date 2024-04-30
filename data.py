678825d3d077b79e9a11420ba5e1a8fdf922e42c
pwuaj <pwuaj@ecelvd406.ece.local>
2024-04-30 21:57:46 +0800

    update

--- data.py
+++ data.py
@@ -154,9 +154,10 @@ for i in ['0', '1']:
     filelist = os.listdir(folder)
     print(filelist)
     filelist = random.shuffle(filelist)
+    print(filelist)
     if i == '0':
         for j in range(484):
-            shutil.copy(os.path.join(folder, file[j]), os.path.join(*['/home/pwuaj/data/RDRraw2/training/0', file[j]]))
+            shutil.copy(os.path.join(folder, filelist[j]), os.path.join(*['/home/pwuaj/data/RDRraw2/training/0', file[j]]))
     elif i == '1':
         for j in range(1348):
-            shutil.copy(os.path.join(folder, file[j]), os.path.join(*['/home/pwuaj/data/RDRraw2/training/1', file[j]]))
+            shutil.copy(os.path.join(folder, filelist[j]), os.path.join(*['/home/pwuaj/data/RDRraw2/training/1', file[j]]))
