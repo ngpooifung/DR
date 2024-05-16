1cf9ee17121b04eabbdbfd4c86c592e4f1fd8ddf
pwuaj <pwuaj@ecelvd406.ece.local>
2024-05-17 04:08:44 +0800

    update

--- data.py
+++ data.py
@@ -154,8 +154,8 @@ for i in ['0', '1']:
     filelist = os.listdir(folder)
     random.shuffle(filelist)
     if i == '0':
-        for j in range(316):
-            shutil.copy(os.path.join(folder, filelist[j]), os.path.join(*['/home/pwuaj/data/RDRraw3/training/0', filelist[j]]))
+        for j in range(121):
+            shutil.copy(os.path.join(folder, filelist[j]), os.path.join(*['/home/pwuaj/data/RDRraw2/training/0', filelist[j]]))
     elif i == '1':
-        for j in range(142):
-            shutil.copy(os.path.join(folder, filelist[j]), os.path.join(*['/home/pwuaj/data/RDRraw3/training/1', filelist[j]]))
+        for j in range(337):
+            shutil.copy(os.path.join(folder, filelist[j]), os.path.join(*['/home/pwuaj/data/RDRraw2/training/1', filelist[j]]))
