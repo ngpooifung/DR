4225b50505d463ad54f3152d100ff27a61bcf223
pwuaj <pwuaj@ecelvd406.ece.local>
2024-05-22 07:58:35 +0800

    udpatre

--- data.py
+++ data.py
@@ -22,12 +22,12 @@ from sklearn.metrics import roc_curve, precision_recall_curve, cohen_kappa_score
 # csv = csv.dropna()
 # predict = np.array(csv.iloc[:,0])
 # label = np.array(csv.iloc[:,1])
-# csv = pd.read_csv('/home/pwuaj/hkust/DR/RDRIS.csv')
+# csv = pd.read_csv('/home/pwuaj/hkust/DR/gradtest.csv')
 # predict = np.array(csv['model output'])
 # label = np.array(csv['True label'])
 # fpr, tpr, thresholds = roc_curve(label, predict, drop_intermediate = False)
 # th = thresholds[np.argmax(tpr-fpr)]
-# predict = (predict > 0.6)*1
+# predict = (predict > 0.27)*1
 # print(th,classification_report(label, predict, digits = 4), roc_auc_score(label, predict))
 #
 #
@@ -154,8 +154,8 @@ for i in ['0', '1']:
     filelist = os.listdir(folder)
     random.shuffle(filelist)
     if i == '0':
-        for j in range(100):
+        for j in range(150):
             shutil.copy(os.path.join(folder, filelist[j]), os.path.join(*['/home/pwuaj/data/grad2/training/1', filelist[j]]))
     elif i == '1':
-        for j in range(100):
+        for j in range(150):
             shutil.copy(os.path.join(folder, filelist[j]), os.path.join(*['/home/pwuaj/data/grad2/training/1', filelist[j]]))
