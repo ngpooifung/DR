e554ad09a3d02e86d67cfe489096239396f63bee
pwuaj <pwuaj@ecelvd406.ece.local>
2024-05-24 04:23:03 +0800

    update

--- data.py
+++ data.py
@@ -22,13 +22,13 @@ from sklearn.metrics import roc_curve, precision_recall_curve, cohen_kappa_score
 # csv = csv.dropna()
 # predict = np.array(csv.iloc[:,0])
 # label = np.array(csv.iloc[:,1])
-# csv = pd.read_csv('/home/pwuaj/hkust/DR/RDRGEI.csv')
-# predict = np.array(csv['model output'])
-# label = np.array(csv['True label'])
-# fpr, tpr, thresholds = roc_curve(label, predict, drop_intermediate = False)
-# th = thresholds[np.argmax(tpr-fpr)]
-# predict = (predict > 0.3)*1
-# print(th,classification_report(label, predict, digits = 4), roc_auc_score(label, predict))
+csv = pd.read_csv('/home/pwuaj/hkust/DR/VTDRtest.csv')
+predict = np.array(csv['model output'])
+label = np.array(csv['True label'])
+fpr, tpr, thresholds = roc_curve(label, predict, drop_intermediate = False)
+th = thresholds[np.argmax(tpr-fpr)]
+predict = (predict > 0.8)*1
+print(th,classification_report(label, predict, digits = 4), roc_auc_score(label, predict))
 #
 #
 # # %% result 0.5 0.35 0.45
@@ -154,8 +154,8 @@ for i in ['0', '1']:
     filelist = os.listdir(folder)
     random.shuffle(filelist)
     if i == '0':
-        for j in range(126):
+        for j in range(252):
             shutil.copy(os.path.join(folder, filelist[j]), os.path.join(*['/home/pwuaj/data/VTDRraw2/training/0', filelist[j]]))
     elif i == '1':
-        for j in range(56):
+        for j in range(112):
             shutil.copy(os.path.join(folder, filelist[j]), os.path.join(*['/home/pwuaj/data/VTDRraw2/training/1', filelist[j]]))
