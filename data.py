c10c1c31f9dd29f012d43b1c26ff30cd051ba3fe
pwuaj <pwuaj@ecelvd406.ece.local>
2024-05-24 04:49:58 +0800

    update

--- data.py
+++ data.py
@@ -22,13 +22,13 @@ from sklearn.metrics import roc_curve, precision_recall_curve, cohen_kappa_score
 # csv = csv.dropna()
 # predict = np.array(csv.iloc[:,0])
 # label = np.array(csv.iloc[:,1])
-csv = pd.read_csv('/home/pwuaj/hkust/DR/VTDRtest.csv')
-predict = np.array(csv['model output'])
-label = np.array(csv['True label'])
-fpr, tpr, thresholds = roc_curve(label, predict, drop_intermediate = False)
-th = thresholds[np.argmax(tpr-fpr)]
-predict = (predict > 0.8)*1
-print(th,classification_report(label, predict, digits = 4), roc_auc_score(label, predict))
+# csv = pd.read_csv('/home/pwuaj/hkust/DR/VTDRGEI.csv')
+# predict = np.array(csv['model output'])
+# label = np.array(csv['True label'])
+# fpr, tpr, thresholds = roc_curve(label, predict, drop_intermediate = False)
+# th = thresholds[np.argmax(tpr-fpr)]
+# predict = (predict > 0.9)*1
+# print(th,classification_report(label, predict, digits = 4), roc_auc_score(label, predict))
 #
 #
 # # %% result 0.5 0.35 0.45
