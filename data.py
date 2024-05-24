35bccd87fa39fa3e1989b98f357b58d95243649c
pwuaj <pwuaj@ecelvd406.ece.local>
2024-05-24 13:42:48 +0800

    update

--- data.py
+++ data.py
@@ -22,13 +22,13 @@ from sklearn.metrics import roc_curve, precision_recall_curve, cohen_kappa_score
 # csv = csv.dropna()
 # predict = np.array(csv.iloc[:,0])
 # label = np.array(csv.iloc[:,1])
-# csv = pd.read_csv('/home/pwuaj/hkust/DR/VTDRGEI.csv')
-# predict = np.array(csv['model output'])
-# label = np.array(csv['True label'])
-# fpr, tpr, thresholds = roc_curve(label, predict, drop_intermediate = False)
-# th = thresholds[np.argmax(tpr-fpr)]
-# predict = (predict > 0.9)*1
-# print(th,classification_report(label, predict, digits = 4), roc_auc_score(label, predict))
+csv = pd.read_csv('/home/pwuaj/hkust/DR/VTDRGEI.csv')
+predict = np.array(csv['model output'])
+label = np.array(csv['True label'])
+fpr, tpr, thresholds = roc_curve(label, predict, drop_intermediate = False)
+th = thresholds[np.argmax(tpr-fpr)]
+predict = (predict > 0.93)*1
+print(th,classification_report(label, predict, digits = 4), roc_auc_score(label, predict))
 #
 #
 # # %% result 0.5 0.35 0.45
@@ -150,12 +150,12 @@ from sklearn.metrics import roc_curve, precision_recall_curve, cohen_kappa_score
 
 # %%
 for i in ['0', '1']:
-    folder = os.path.join('/home/pwuaj/data/fundus/VTDR', i)
+    folder = os.path.join('/home/pwuaj/data/fundus/RDR', i)
     filelist = os.listdir(folder)
     random.shuffle(filelist)
     if i == '0':
-        for j in range(252):
-            shutil.copy(os.path.join(folder, filelist[j]), os.path.join(*['/home/pwuaj/data/VTDRraw2/training/0', filelist[j]]))
+        for j in range(100):
+            shutil.copy(os.path.join(folder, filelist[j]), os.path.join(*['/home/pwuaj/data/grad3/training/1', filelist[j]]))
     elif i == '1':
-        for j in range(112):
-            shutil.copy(os.path.join(folder, filelist[j]), os.path.join(*['/home/pwuaj/data/VTDRraw2/training/1', filelist[j]]))
+        for j in range(100):
+            shutil.copy(os.path.join(folder, filelist[j]), os.path.join(*['/home/pwuaj/data/grad3/training/1', filelist[j]]))
