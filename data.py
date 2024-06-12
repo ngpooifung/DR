abb915dc1915665f3e15bc9c5a2bd39d05c94e3f
pwuaj <pwuaj@ecelvd406.ece.local>
2024-06-12 18:20:38 +0800

    update

--- data.py
+++ data.py
@@ -27,7 +27,7 @@ from sklearn.metrics import roc_curve, precision_recall_curve, cohen_kappa_score
 # label = np.array(csv['True label'])
 # fpr, tpr, thresholds = roc_curve(label, predict, drop_intermediate = False)
 # th = thresholds[np.argmax(tpr-fpr)]
-# predict = (predict > 0.5)*1
+# predict = (predict > 0.4)*1
 # print(th,classification_report(label, predict, digits = 4), roc_auc_score(label, predict))
 
 # %% result 0.5 0.35 0.45
@@ -153,8 +153,8 @@ for i in ['0', '1']:
     filelist = os.listdir(folder)
     random.shuffle(filelist)
     if i == '0':
-        for j in range(32):
+        for j in range(121):
             shutil.copy(os.path.join(folder, filelist[j]), os.path.join(*['/home/pwuaj/data/RDRraw/training2/0', filelist[j]]))
     elif i == '1':
-        for j in range(90):
+        for j in range(337):
             shutil.copy(os.path.join(folder, filelist[j]), os.path.join(*['/home/pwuaj/data/RDRraw/training2/1', filelist[j]]))
