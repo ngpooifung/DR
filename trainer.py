70573c0eff16e8fbbd2976503527fb373c179a47
pwuaj <pwuaj@ecelvd406.ece.local>
2024-07-08 06:27:50 +0800

    update

--- trainer.py
+++ trainer.py
@@ -15,7 +15,7 @@ import scipy
 import logging
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
-import umap.umap_ as umap
+import umap
 from tqdm import tqdm
 import pandas as pd
 from sklearn.metrics import accuracy_score
