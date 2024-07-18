f0b4d9485542bd9a301559aa88f39357771d513e
pwuaj <pwuaj@ecelvd406.ece.local>
2024-07-18 20:37:40 +0800

    updte

--- trainer.py
+++ trainer.py
@@ -389,6 +389,7 @@ class Restrainer(object):
             grayscale_cams = cam(image.to(self.args.device), targets=targets)
             img = Image.open(path)
             img = data_transforms(img)
+            print(imag.shape, grayscale_cams[0, :].shape)
             cam_image = show_cam_on_image(img, grayscale_cams[0, :], use_rgb=True)
 
             if lbl.item() ==1:
