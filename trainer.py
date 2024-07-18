5678345cdac1178be45bf12297a46d2cbfe29be1
pwuaj <pwuaj@ecelvd406.ece.local>
2024-07-18 20:55:50 +0800

    updte

--- trainer.py
+++ trainer.py
@@ -389,8 +389,7 @@ class Restrainer(object):
             grayscale_cams = cam(image.to(self.args.device), targets=targets)
             img = Image.open(path)
             img = data_transforms(img)
-            print(img.dtype, grayscale_cams.dtype)
-            cam_image = show_cam_on_image(img, grayscale_cams[0, :], use_rgb=True)
+            cam_image = show_cam_on_image(np.asarray(img).squeeze().transpose(1,2,0), grayscale_cams[0, :], use_rgb=True)
 
             if lbl.item() ==1:
                 images = np.hstack((img, cam_image))
