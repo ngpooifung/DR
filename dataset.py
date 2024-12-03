080331bf747a60eeabcda7bb1a8518ea26359827
pwuaj <pwuaj@ecelvd406.ece.local>
2024-12-03 21:24:28 +0800

    update

--- dataset.py
+++ dataset.py
@@ -19,10 +19,9 @@ except ImportError:
 def claheimg(img):
     clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
     lab_img = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
-    lab_planes = cv2.split(lab_img)
-    lab_planes[0] = clahe.apply(lab_planes[0])
-    lab_img = cv2.merge(lab_planes)
-    print(good)
+    l,a,b = lab_img[:,:0], lab_img[:,:,1], lab_img[:,:,2]
+    l = clahe.apply(l)
+    lab_img = np.dstack((l,a,b))
     return cv2.cvtColor(lab_img, cv2.COLOR_LAB2RGB)
 
 class Imagefolder(datasets.ImageFolder):
